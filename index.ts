9407fc2ca53fc69da538e8a2e9c3c1d77bc52d73
Martha Durand <martha.mdurand@gmail.com>
2026-05-19 17:19:00 +0200

    modification des log

--- index.ts
+++ index.ts
@@ -1,56 +1,88 @@
 import type { ProcessingConfig } from './types/processingConfig/index.ts'
 import type { ProcessingContext } from '@data-fair/lib-common-types/processings.js'
 
-import datasetSchema from './lib/schema/dataset-schema.ts'
+// import datasetSchema from './lib/schema/dataset-schema.ts'
 import datasetSchemaMarche from './lib/schema/dataset-schema-marche.ts'
 import datasetSchemaConcession from './lib/schema/dataset-schema-concession.ts'
 import dayjs from 'dayjs'
+import fs from 'fs-extra'
 import { urlDecp } from './lib/url.ts'
-import { initializeWithAnnualDecp, initializeWithGlobalDecp, dailyDecp } from './lib/execute.ts'
+import { getAttachement } from './lib/fetch.ts'
+import { initializeWithGlobalDecp, dailyDecp } from './lib/execute.ts'
 
 // List of mapping available
-import mapping from './lib/mapping/mapping_decp.json' with { type: 'json' }
+// import mapping from './lib/mapping/mapping_decp.json' with { type: 'json' }
 import mappingMarche from './lib/mapping/mapping_decp_marche.json' with { type: 'json' }
 import mappingConcession from './lib/mapping/mapping_decp_concession.json' with { type: 'json' }
 
 export const run = async (context: ProcessingContext<ProcessingConfig>) => {
-  const { processingConfig, log, axios, processingId, patchConfig } = context
+  const { processingConfig, log, axios, processingId, tmpDir, patchConfig } = context
   const create = processingConfig.datasetMode === 'create'
   const update = processingConfig.datasetMode === 'update'
-  const initialize = processingConfig.initializeDataset
   const filtersConcession = ['marches.contrat-concession']
   const filtersMarche = ['marches.marche']
-  const primaryKeyConcession = ['id', 'valeurglobal', 'autoriteconcedanteid', 'concessionnairessiret', 'objet']
-  const primaryKeyMarche = ['id', 'montant', 'acheteurid', 'titulairesiret', 'objet']
-  const extensions = [
-    {
-      active: true,
-      type: 'remoteService',
-      remoteService: 'koumoul-com-dataset-sirene',
-      action: 'masterData_bulkSearch_siret-infos',
-      select: [
-        'denominationUniteLegale',
-        '_siret_coords.y_latitude',
-        '_siret_coords.x_longitude',
-        '_infos_commune.code_departement',
-        '_infos_commune.code_region'
-      ],
-      overwrite: {},
-      propertyPrefix: '_siret_infos'
-    }
-  ]
 
   if (create) {
     const titleBase = processingConfig.datasetTitle as string
-    await log.step(`Creating dataset "${titleBase}"`)
-    // -- Récupération des condition de filtrage pour savoir combien de tableau créer
-    const marcheFilter = processingConfig.datasetFilterCreate === 'all' || processingConfig.datasetFilterCreate === 'marche'
-    const marcheConcession = processingConfig.datasetFilterCreate === 'all' || processingConfig.datasetFilterCreate === 'concession'
-    await log.info(`Filtre marché : "${marcheFilter}"`)
-    await log.info(`Filtre concession : "${marcheConcession}"`)
+    const initialize = processingConfig.initializeDataset
+    const extensions = [
+      {
+        active: true,
+        type: 'remoteService',
+        remoteService: 'dataset:geolocalisation-des-etablissements-du-repertoire-sirene',
+        action: 'masterData_bulkSearch_siret-coords',
+        select: [
+          'x_longitude',
+          'y_latitude'
+        ],
+        overwrite: {},
+        propertyPrefix: '_siret_coords'
+      },
+      {
+        active: true,
+        type: 'remoteService',
+        remoteService: 'dataset:sirene',
+        action: 'masterData_bulkSearch_siret-infos',
+        select: [
+          '_infos_commune.code_departement',
+          '_infos_commune.code_region',
+          'denominationUniteLegale'
+        ],
+        overwrite: {},
+        propertyPrefix: '_siret_infos'
+      }
+    ]
+    // const extensions = [
+    //   {
+    //     active: true,
+    //     type: 'remoteService',
+    //     remoteService: 'koumoul-com-dataset-sirene',
+    //     action: 'masterData_bulkSearch_siret-infos',
+    //     select: [
+    //       'denominationUniteLegale',
+    //       '_siret_coords.y_latitude',
+    //       '_siret_coords.x_longitude',
+    //       '_infos_commune.code_departement',
+    //       '_infos_commune.code_region'
+    //     ],
+    //     overwrite: {},
+    //     propertyPrefix: '_siret_infos'
+    //   }
+    // ]
+
+    const all = processingConfig.datasetFilterCreate === 'all'
+    const marcheFilter = all || processingConfig.datasetFilterCreate === 'marche'
+    const marcheConcession = all || processingConfig.datasetFilterCreate === 'concession'
+    let pathDECP
+    log.step(all ? 'Debut de la création des jeux de données' : 'Début de la création du jeu de donnée')
+    if (initialize) {
+      log.info('Récupération des données d\'initialisation sur data.gouv')
+      pathDECP = await getAttachement(urlDecp.DECP_GLOBAL, tmpDir, 'global.json', axios)
+    }
 
     // Filtrer la concessions
     if (marcheConcession) {
+      const primaryKeyConcession = ['id', 'valeurglobal', 'autoriteconcedanteid', 'concessionnairessiret', 'objet']
       const title = titleBase + ' (concession)'
 
       const dataset = (await axios.post('api/v1/datasets', {
@@ -61,20 +93,31 @@ export const run = async (context: ProcessingContext<ProcessingConfig>) => {
         extensions,
         extras: { processingId }
       })).data
-      await log.info(`Dataset created, id="${dataset.id}", title="${dataset.title}"`)
-      await log.info('Dataset créé/récupéré avec extensions:', dataset.extensions)
-      if (initialize) {
+      await log.step(`Création du nouveau jeu de donnée : "${dataset.title}"`)
+      if (initialize && pathDECP) {
         // await initializeWithAnnualDecp(mappingConcession, filtersConcession, context)
-        await initializeWithGlobalDecp(mappingConcession, filtersConcession, urlDecp.DECP_GLOBAL, dataset.id, context)
-      } else {
-        log.info('Aucune initialisation requise')
+        await initializeWithGlobalDecp(mappingConcession, filtersConcession, pathDECP, dataset.id, context)
       }
       if (!marcheFilter) {
         await patchConfig({ datasetMode: 'update', dataset, datasets: undefined } as any)
       }
     }
-
     if (marcheFilter) {
+      const primaryKeyMarche = ['id', 'montant', 'acheteurid', 'titulairesiret', 'objet']
+      const extensionCPV = {
+        active: true,
+        type: 'remoteService',
+        remoteService: 'dataset:a5u3jsc115-84-1c2591opar',
+        action: 'masterData_bulkSearch_nomenclature-des-secteurs-dachat-code-cpv',
+        select: [
+          'secteurs',
+          'soussecteurs',
+          'intitule_officiel_par_code_cpv'
+        ],
+        overwrite: {},
+        propertyPrefix: '_nomenclature_des-secteurs-dachat-code-cpv'
+      }
+      extensions.push(extensionCPV)
       const title = titleBase + ' (marché)'
 
       const dataset = (await axios.post('api/v1/datasets', {
@@ -85,34 +128,34 @@ export const run = async (context: ProcessingContext<ProcessingConfig>) => {
         extensions,
         extras: { processingId }
       })).data
-      await log.info(`Dataset created, id="${dataset.id}", title="${dataset.title}"`)
+      await log.info(`Création du nouveau jeu de donnée : "${dataset.title}"`)
 
-      if (initialize) {
+      if (initialize && pathDECP) {
         // await initializeWithAnnualDecp(mapping, filtersMarche, context)
-        await initializeWithGlobalDecp(mappingMarche, filtersMarche, urlDecp.DECP_GLOBAL, dataset.id, context)
-      } else {
-        log.info('Aucune initialisation requise')
+        await initializeWithGlobalDecp(mappingMarche, filtersMarche, pathDECP, dataset.id, context)
       }
       await patchConfig({ datasetMode: 'update', dataset, datasets: undefined } as any)
     }
+    log.step('fin de l\'initialisation')
   }
 
   if (update) {
-    log.step('Lancement traitement de test')
+    log.step('Lancement de la mise à jour')
     const marcheFilter = processingConfig.datasetFilterUpdate === 'marche'
     const marcheConcession = processingConfig.datasetFilterUpdate === 'concession'
     const date = processingConfig._overrideDate as string ?? dayjs().format('YYYY-MM-DD')
     // récupération de l'id du jeu de donnée
     const ref = processingConfig.dataset
-    log.step(`Checking dataset "${ref.title || ref.id}"`)
+    log.info(`Récupération du dataset "${ref.title || ref.id}"`)
 
     const datasetId = processingConfig.dataset.id as string
-
+    log.step('Récupérarion du decp du jour')
     if (marcheFilter) {
       await dailyDecp(mappingMarche, filtersMarche, date, datasetId, context)
     }
     if (marcheConcession) {
       await dailyDecp(mappingConcession, filtersConcession, date, datasetId, context)
     }
+    log.step('fin de l\'update')
   }
 }
--- lib/execute.ts
+++ lib/execute.ts
@@ -6,73 +6,59 @@ import dayjs from 'dayjs'
 import { sendFlattenData } from './upload.ts'
 
 // Importe decp global
-export const initializeWithGlobalDecp = async (mapping: any[], filters: string[], urlGlobal: string, datasetId: string, context: ProcessingContext<ProcessingConfig>) => {
-  const { log, tmpDir, axios, processingConfig } = context
-  const patternGlobal = /^decp-global\.json$/  // le regex est faux
+export const initializeWithGlobalDecp = async (mapping: any[], filters: string[], pathDECP: string, datasetId: string, context: ProcessingContext<ProcessingConfig>) => {
+  const { log, processingConfig } = context
+  const patternGlobal = /^decp-global\.json$/
   const list: any[] = await listAttachements(context, patternGlobal)
-  console.log(JSON.stringify(list, null, 2))
   const lastUpdate = list[0].date
-  log.warning(`Dernière mise à jour : ${lastUpdate}`)
+  log.info(`Dernière mise à jour du decp global : ${lastUpdate}`)
   const dates = getDatesFrom(lastUpdate, processingConfig._overrideDate as string)
-  const path = await getAttachement(urlGlobal, tmpDir, axios)
-  log.info('Envoie de la donnée')
+
   for (const filter of filters) {
-    log.info(`envoie des données du filtre ${filter}`)
-    await sendFlattenData(mapping, filter, path, datasetId, context)
+    await sendFlattenData('DECP global', mapping, filter, pathDECP, datasetId, context)
   }
-  log.info('Supprimer du dossier local')
-  await fs.promises.unlink(path)
   for (const date of dates) {
-    log.info(`envoie des d'api decp à la date du ${date}`)
     await dailyDecp(mapping, filters, date, datasetId, context)
   }
-  log.step('fin de l\'initialisation')
 }
 
-export const initializeWithAnnualDecp = async (mapping: any[], filters: string[], context: ProcessingContext<ProcessingConfig>) => {
-  const { log, tmpDir, axios } = context
-  const patternYear = /^decp-\d{4}.json$/
+// export const initializeWithAnnualDecp = async (mapping: any[], filters: string[], context: ProcessingContext<ProcessingConfig>) => {
+//   const { log, tmpDir, axios } = context
+//   const patternYear = /^decp-\d{4}.json$/
 
-  // récupérer tous les decp
-  const list: any[] = await listAttachements(context, patternYear)
-  // faire une boucle pour chaque decp
-  for (const e of list) {
-    log.step(`Traitement de ${e.title}`)
-    log.info('Lancement du téléchargement')
-    const path = await getAttachement(e.url, tmpDir, axios)
-    log.info('Envoie de la donnée')
-    // Téléchargement de tous les decp annuel
-    for (const filter of filters) {
-      log.info(`envoie des données du filtre ${filter}`)
-      await sendFlattenData(mapping, filter, path, context.processingId, context)
-    }
+//   // récupérer tous les decp
+//   const list: any[] = await listAttachements(context, patternYear)
+//   // faire une boucle pour chaque decp
+//   for (const e of list) {
+//     log.step(`Traitement de ${e.title}`)
+//     log.info('Lancement du téléchargement')
+//     const path = await getAttachement(e.url, tmpDir, axios)
+//     log.info('Envoie de la donnée')
+//     // Téléchargement de tous les decp annuel
+//     for (const filter of filters) {
+//       log.info(`envoie des données du filtre ${filter}`)
+//       await sendFlattenData(mapping, filter, path, context.processingId, context)
+//     }
 
-    // Télécharger d'api decp pour récupèrer les dates après la dernière modification
+//     log.info(`Supprimer ${e.title} du dossier local`)
+//     await fs.promises.unlink(path)
+//   }
 
-    log.info(`Supprimer ${e.title} du dossier local`)
-    await fs.promises.unlink(path)
-  }
-
-  log.step('fin de l\'initialisation')
-}
+//   log.step('fin de l\'initialisation')
+// }
 
 export const dailyDecp = async (mapping: any[], filters: string[], date: string, datasetId: string, context: ProcessingContext<ProcessingConfig>) => {
   const { log, tmpDir, axios } = context
-  log.info(`Récupérarion de l'url du ${date}`)
   const url = await getDailyAttachement(date, context)
   if (!url.length) {
-    log.info(`No resource found for date ${date}, skipping`)
+    log.info(`Aucun decp publié le ${date}`)
     return
   }
-  const path = await getAttachement(url.toString(), tmpDir, axios)
-  log.info('Envoie de la donnée')
+  const path = await getAttachement(url.toString(), tmpDir, `${date}.json`, axios)
   for (const filter of filters) {
-    log.info(`envoie des données du filtre ${filter}`)
-    await sendFlattenData(mapping, filter, path, datasetId, context)
+    await sendFlattenData(date, mapping, filter, path, datasetId, context)
   }
-  log.info('Suppression du dossier local')
   await fs.promises.unlink(path)
-  log.step('fin de l\'update')
 }
 
 const getDatesFrom = (from: string, to?: string): string[] => {
